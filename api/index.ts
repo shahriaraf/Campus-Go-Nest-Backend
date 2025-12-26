8a847ae5d2fbc178377752124287f2d1924adab6
Shoumo Shahriar Araf <shahriaraf01@gmail.com>
2025-12-26 23:30:11 +0600

    deployed

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,39 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
-import * as express from 'express';
+import express, { Request, Response } from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
+// 1. Create Express instance outside the handler
 const server = express();
 
-export const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+// 2. Cache the Nest Promise
+let nestHandler: Promise<any>;
+let isInitialized = false;
 
-  // Enable CORS so your Frontend can talk to this
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // 3. Add same settings as main.ts
   app.enableCors({
-    origin: '*', // For production, change this to your Vercel Frontend URL
+    origin: '*',
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.init();
+  return server;
 };
 
-createNestServer(server)
-  .then(() => console.log('Nest Ready'))
-  .catch((err) => console.error('Nest broken', err));
-
-export default async function handler(req, res) {
-  await createNestServer(server);
+// 4. Export the Vercel Handler
+export default async function handler(req: Request, res: Response) {
+  if (!isInitialized) {
+    isInitialized = true;
+    nestHandler = bootstrap();
+  }
+  // Wait for Nest to be ready
+  await nestHandler;
+  // Pass request to Express
   server(req, res);
 }
